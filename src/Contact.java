1982821d47187e8a13ffa58caeabba9b24985866
mario welch <mario.welch01@gmail.com>
2022-01-14 11:56:15 -0600

    created contact, contact app class

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,25 @@
 public class Contact {
-    public static void main(String[] args) {
-        System.out.println("run");
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
---
+++ src/ContactApp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ContactApp {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.println("Main Menu");
+
+        System.out.println("1. View contacts...");
+        System.out.println("2. Add a new contact...");
+        System.out.println("3. Search a contact by name...");
+        System.out.println("4. Delete an existing contact...");
+        System.out.println("5. Exit...");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5");
+
+
+    }
+}
