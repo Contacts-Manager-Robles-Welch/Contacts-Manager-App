7f166a907ce455ce500e7a4a9ed74d5dd0f61d68
mario welch <mario.welch01@gmail.com>
2022-01-14 13:47:27 -0600

    ability to print contacts.txt to console

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -1,18 +1,53 @@
+import util.Input;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class ContactApp {
     public static Scanner sc = new Scanner(System.in);
 
+    //Prints contacts to console from contacts.txt
+    public static void printContacts(){
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        try{
+            List<String> contacts = Files.readAllLines(dataFile);
+            for(String contact : contacts){
+                System.out.println(contact);
+            }
+//            System.out.println(contacts);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args){
         System.out.println("Main Menu");
 
-        System.out.println("1. View contacts...");
-        System.out.println("2. Add a new contact...");
-        System.out.println("3. Search a contact by name...");
-        System.out.println("4. Delete an existing contact...");
+        System.out.println("1. View All Contacts...");
+        System.out.println("2. Add A New Contact...");
+        System.out.println("3. Search A Contact By Name...");
+        System.out.println("4. Delete An Existing Contact...");
         System.out.println("5. Exit...");
-        System.out.println("Enter an option (1, 2, 3, 4 or 5");
+//        System.out.println("Enter an option 1, 2, 3, 4 or 5");
 
+        Input input = new Input();
+        //uses util/input method
+        int userInput = input.getInt("Enter a number between 1 and 5", 1, 5);
 
+        switch(userInput){
+            case 1:
+                printContacts();
+                break;
+            default:
+                System.out.println("Not an Option");
+        }
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -6,18 +6,12 @@ public class Input {
 
         private Scanner scanner;
 
-        //Constructor, have the same name as the class and do not define a return type
-        //They can accept arguments and be overloaded.
         public Input() {
             this.scanner = new Scanner(System.in);
         }
 
-        //Class will have the following methods, which all will return command line input from the user
-        //String getString(), boolean yesNo(), int getInt(int min, int max), int getInt(), double getDouble(double min, double max), double getDouble()
         public String getString(){
-//        System.out.println("Enter Something: ");
             String userInput =  scanner.nextLine();
-//        System.out.printf("You entered: %s %n", userInput);
             return userInput;
         }
 
@@ -26,72 +20,32 @@ public class Input {
             return scanner.nextLine();
         }
 
-        public boolean yesNo(){
-            System.out.println("Would you like to continue?");
-            String userInput = scanner.nextLine();
-            if(userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")){
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-//    public int getInt(){
-//        System.out.println("Enter a whole number: ");
-//        int userInput = scanner.nextInt();
-//        System.out.printf("You entered %d: ", userInput);
-//        return userInput;
-//    }
 
         public int getInt(){
-            System.out.println("Enter a Integer: ");
             try{
-                return Integer.valueOf(this.getString());
+                return Integer.parseInt(this.getString());
             } catch(NumberFormatException nfe){
-//            nfe.printStackTrace();
+                //nfe.printStackTrace();
                 System.out.println("This is not a integer. Please enter a Integer!");
-                return getInt();
             }
+            return getInt();
         }
 
-        public int getInt(int min, int max){
-            System.out.printf("Enter a number between %d and %d. %n", min, max);
+
+        public int getInt(String prompt, int min, int max){
+            System.out.println(prompt);
             int userInput = scanner.nextInt();
 
-            if(userInput >= min && userInput <= max){
-                return userInput;
-            }
-            System.out.printf("Number is not between %d and %d. Please enter a correct number! %n", min, max);
-            return getInt(min, max);
-        }
-
-//    public double getDouble(){
-//        System.out.println("Enter a number: ");
-//        double userInput = scanner.nextDouble();
-//        System.out.printf("You entered %f: ", userInput);
-//        return userInput;
-//    }
-
-        public double getDouble(){
-            System.out.println("Enter a decimal: ");
             try{
-                return Double.valueOf(this.getString());
+                if(userInput < min && userInput > max) {
+                    return Integer.parseInt(this.getString());
+                }
             } catch(NumberFormatException nfe){
-//            nfe.printStackTrace();
-                System.out.println("This is not a decimal. Please enter a decimal!");
-                return getDouble();
+                //nfe.printStackTrace();
+                System.out.printf("Number is not between %d and %d. Please enter a correct number! %n", min, max);
+                return getInt(prompt, min, max);
             }
+            return userInput;
         }
 
-        public double getDouble(double min, double max){
-            System.out.printf("Enter a number between %f and %f. %n", min, max);
-            double userInput = scanner.nextDouble();
-
-            if(userInput >= min && userInput <= max){
-                return userInput;
-            }
-            System.out.printf("Number is not between %f and %f. Please enter a correct number! %n", min, max);
-            return getDouble(min, max);
-        }
-
-    }
+}
